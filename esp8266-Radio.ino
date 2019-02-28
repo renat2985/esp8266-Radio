0c273b01b992491dc6c7adcaac6921c1ce48b085
renat2985 <wul@inbox.lv>
2019-02-28 16:14:48 +0200

    Стало лучше

--- esp8266-Radio.ino
+++ esp8266-Radio.ino
@@ -16,17 +16,17 @@ WiFiClient wclient;
 // To run, set your ESP8266 build to 160MHz, update the SSID info, and upload.
 
 // Randomly picked URL
-  //http://kpradio.hostingradio.ru:8000/
-  //http://radio.horoshee.fm:8000/mp3
-  //http://kpradio.hostingradio.ru:8000/russia.radiokp128.mp3
-  //const char *URL = "http://ic6.101.ru:8000/p933259";
-  //const char *URL = "http://icecast.russkoeradio.cdnvideo.ru:8000/rr_m.mp3";
-  //const char *URL = "http://music.myradio.ua/rep128.mp3";
-  //const char *URL = "http://jazz.streamr.ru/jazz-128.mp3";
-  //const char *URL = "http://87.110.219.34:8000/swhmp3";
-  //const char *URL = "http://87.110.219.34:8000/plusmp3";
-  //const char *URL = "http://91.90.255.111:80/MixFM_96"; //GOOD
-  //const char *URL = "https://nashe1.hostingradio.ru:18000/nashe20-64.mp3";
+//http://kpradio.hostingradio.ru:8000/
+//http://radio.horoshee.fm:8000/mp3
+//http://kpradio.hostingradio.ru:8000/russia.radiokp128.mp3
+//const char *URL = "http://ic6.101.ru:8000/p933259";
+//const char *URL = "http://icecast.russkoeradio.cdnvideo.ru:8000/rr_m.mp3";
+//const char *URL = "http://music.myradio.ua/rep128.mp3";
+//const char *URL = "http://jazz.streamr.ru/jazz-128.mp3";
+//const char *URL = "http://87.110.219.34:8000/swhmp3";
+//const char *URL = "http://87.110.219.34:8000/plusmp3";
+//const char *URL = "http://91.90.255.111:80/MixFM_96"; //GOOD
+//const char *URL = "https://nashe1.hostingradio.ru:18000/nashe20-64.mp3";
 String URLs= "http://jazz.streamr.ru/jazz-64.mp3";
 AudioGeneratorMP3 *mp3;
 AudioFileSourceICYStream *file;
@@ -34,15 +34,19 @@ AudioFileSourceBuffer *buff;
 AudioOutputI2SNoDAC *out;
 
 void setup(){
-   system_update_cpu_freq(SYS_CPU_160MHZ);
-  Serial.begin(115200);
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("radio");
-  Serial.println("connected...yeey :)");
-  String doneURL = getURL("http://backup.privet.lv/radio/radio.txt");
-  if (doneURL != "") URLs = doneURL;
-  Serial.println(URLs);
-  AUDIO_init();
+ system_update_cpu_freq(SYS_CPU_160MHZ);
+ Serial.begin(115200);
+ WiFiManager wifiManager;
+ wifiManager.autoConnect("radio");
+ Serial.println("connected...yeey :)");
+ String doneURL = getURL("http://backup.privet.lv/radio/"+WiFi.macAddress()+".txt");
+ Serial.println(WiFi.macAddress());
+ if (doneURL == "") {
+  doneURL = getURL("http://backup.privet.lv/radio/radio.txt");
+ }
+ if (doneURL != "") URLs = doneURL;
+ Serial.println(URLs);
+ AUDIO_init();
 }
 
 void loop(){
@@ -51,13 +55,13 @@ void loop(){
 
 // ------------- Запрос на удаленный URL -----------------------------------------
 String getURL(String urls) {
-  String answer;
-  HTTPClient http;
-  http.begin(urls); //HTTP
-  int httpCode = http.GET();
-  if (httpCode == HTTP_CODE_OK) { // если сервер вернул данные
-    answer = http.getString(); // Данные здесь
-  }
-  http.end();
-  return answer;
+ String answer;
+ HTTPClient http;
+ http.begin(urls); //HTTP
+ int httpCode = http.GET();
+ if (httpCode == HTTP_CODE_OK) { // если сервер вернул данные
+  answer = http.getString(); // Данные здесь
+ }
+ http.end();
+ return answer;
 }
